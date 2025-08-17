45b4d683d7cb24630815774bfc425080c24f7f3f
Julien ouellet <julienouellet@iusevimbtw.com>
2025-08-17 11:36:26 -0400

    inital commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Sportlight local development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        sqlite
+        air
+      ];
+
+      shellHook = ''
+        echo "----------------------------"
+        echo "Development environment ready"
+        echo "Go version: $(go version)"
+        echo "SQLite version: $(sqlite3 --version)"
+        echo "Air version: $(air -v)"
+        echo "----------------------------"
+      '';
+    };
+  };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JulOuellet/sportlight
+
+go 1.24.5
