9dde3c824c8165ac196ada6ef723677289bbb52e
Julien ouellet <julienouellet@iusevimbtw.com>
2025-08-23 16:25:13 -0400

    add migration tool to flake

--- flake.nix
+++ flake.nix
@@ -19,16 +19,22 @@
         docker
         docker-compose
         postgresql
-        go-migrate
       ];
 
       shellHook = ''
+        export PATH="$HOME/go/bin:$PATH"
+
+        if ! command -v migrate &> /dev/null; then
+          echo "Installing golang-migrate..."
+          go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+        fi
+
+
         echo "----------------------------"
         echo "Development environment ready"
         echo "Go version: $(go version)"
         echo "Docker version: $(docker --version)"
         echo "Docker Compose version: $(docker-compose --version)"
-        echo "Air version: $(air -v)"
         echo "----------------------------"
       '';
     };
